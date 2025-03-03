de2bdde6e522fb3c7a02aa21f4ae15cc2d31fa9a
Shreeharini <shreeharini699@gmail.com>
2025-03-03 17:32:25 +0530

    Create Pascal's Triangle.C

---
+++ Pascal's Triangle.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int rows;
+    printf("Enter the number of rows\n");
+    scanf("%d",&rows);
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<rows-i-1;j++){
+            printf(" ");
+        }
+  for(int k=0;k<=i;k++){
+      printf("* ");
+      if(k != i){
+          printf(" ");
+      }
+  } 
+  printf("\n");
+    }
+    return 0;
+}
